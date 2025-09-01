8672c39072281a5ce3eb882d280070ab414006c7
K Poorna Sai <poornasaikorlakunta1@gmail.com>
2025-09-01 15:25:33 +0530

    Add files via upload

---
+++ basic mathematical operations.py
@@ -0,0 +1,15 @@
+x=input("Enter the first number: ")
+y=input("Enter the second number: ")
+
+x=int(x)
+y=int(y)
+
+add=(x+y)
+sub=(x-y)
+mul=(x*y)
+div=(x/y)
+
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
